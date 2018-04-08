5f3c5fbb6bf196ef8c9a7391457cc51518bdcbc6
TimVan <877020296@qq.com>
2018-04-08 11:31:07 +0800

    pb1372

--- 省赛/Java/src/com/company/PB1372.java
+++ 省赛/Java/src/com/company/PB1372.java
@@ -1,8 +1,30 @@
 package com.company;
+/*http://115.159.198.87:8080/problemset/problem/1372*/
+/*
+* 第一行输入正整数T，表示数据的组数。
+每组数据的第一行是购买的物品的种类数n 和顾客给的钱的总数m(顾客给的钱不会有"分"),接下来有n行[1,100],每行两个数字,第一个数字是该物品的价格Pi([0.01,10000]),第二个数字是该物品的数量Ci([1,10])。
+*/
+
+import java.util.Scanner;
 
 public class PB1372 {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int T = 0, n = 0;
+        double  m = 0;
+        Scanner sc = new Scanner(System.in);
+
+
+        for (int T_i = 0 ; T_i < T ; ++T_i){
+            n = sc.nextInt();
+            m = sc.nextDouble();
+
+            for (int n_i = 0 ; n_i < n ; ++n_i){
+
+
+            }
+        }
+
     }
 }
