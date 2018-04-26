c5276f096fa8f9b46e1b603302145fbb8ce623a2
TimVan <877020296@qq.com>
2018-04-27 01:55:48 +0800

    新建 pb1372.cpp

---
+++ 省赛/Java/src/com/company/pb1372.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+//第一行输入正整数T，表示数据的组数。
+//每组数据的第一行是购买的物品的种类数n 和顾客给的钱的总数m(顾客给的钱不会有"分"),
+// 接下来有n行[1,100],每行两个数字,第一个数字是该物品的价格Pi([0.01,10000]),第二个数字是该物品的数量Ci([1,10])。
+
+int main() {
+    int N = 0;
+    cin>>N;
+    for (int sume_i = 0; sume_i < N ; ++sume_i) {
+        int n = 0;
+        double sum_price = 0;
+        cin>>n>>sum_price;
+        for (int i = 0; i < ; ++i) {
+
+        }
+    }
+
+    return 0;
+}
+
