1be5a7b676aa4b34e87b6d759d9cb50f1296db2e
TimVan <877020296@qq.com>
2019-03-04 15:01:24 +0800

    jdbc

--- 蓝桥杯/Java/src/com/timvan/Java方向班/Char集合网络JDBC0225/MyJDBCLearn.java
+++ 蓝桥杯/Java/src/com/timvan/Java方向班/Char集合网络JDBC0225/MyJDBCLearn.java
@@ -8,10 +8,15 @@ package com.timvan.Java方向班.Char集合网络JDBC0225;
  * @date : 2019-03-04 09:06
  **/
 public class MyJDBCLearn {
-    String URL = "jdbc:mysql://localhost:3306/memex";
-    String USER = "root";
-    String PASSWORD = "";
-    String DRIVER = "mysql-connector-java-8.0.15.jar";
+    final static String URL = "jdbc:mysql://localhost:3306/memex";
+    final static  String USER = "root";
+    final static  String PASSWORD = "";
+    final static  String DRIVER = "com.mysql.cj.jdbc.Driver";
+
+    public MyJDBCLearn() {
+
+
+    }
 
 
     /**
@@ -25,7 +30,15 @@ public class MyJDBCLearn {
      * 6、处理异常,关闭JDBC对象资源
      * */
 
+
+
     public static void main(String[] args) {
+
+        try {
+            Class.forName(DRIVER);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
 
 
     }
