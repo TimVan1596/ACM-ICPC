af7f4335e4a4edb9927c03b1d736f1cbf2dc5326
TimVan <877020296@qq.com>
2019-03-04 10:45:02 +0800

    搭建 MySQL 环境

---
+++ 蓝桥杯/Java/src/com/timvan/Java方向班/Char集合网络JDBC0225/MyJDBCLearn.java
@@ -0,0 +1,32 @@
+package com.timvan.Java方向班.Char集合网络JDBC0225;
+
+/**
+ * <h3>ACM</h3>
+ * <p>JDBC数据库练习</p>
+ *
+ * @author : TimVan
+ * @date : 2019-03-04 09:06
+ **/
+public class MyJDBCLearn {
+    String URL = "";
+    String USER = "";
+    String PASSWORD = "";
+    String DRIVER = "mysql-connector-java-8.0.15.jar";
+
+
+
+    /**
+     * JDBC连接数据库的6个步骤
+     *
+     * 1、配置URL、用户、密码连接信息
+     * 2、注册JDBC驱动
+     * 3、向DriverManager请求,获得Connection对象
+     * 4、创建PrepareStatement对象
+     * 5、执行SQL、查询结果集
+     * 6、处理异常,关闭JDBC对象资源
+     * */
+
+    public static void main(String[] args) {
+
+    }
+}
