784c903987e09811b392871922d962164b20cf89
TimVan <877020296@qq.com>
2019-03-22 23:32:27 +0800

    最长子串

---
+++ 蓝桥杯/算法训练/【柳神】算法训练_字符统计（最长子串统计）.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+
+using namespace std;
+
+int N;
+string str;
+
+int main(void) {
+	int len;
+	int Maxcnt = 0;
+	string ans, s;
+
+
+//	cin >> N >> str;
+	N = 4;
+	str = "bbaabbaaaaa";
+	len = str.size();
+
+
+	for(int k = len; k >= N; --k) {
+		for(int i = 0; i < len - k; ++i) {
+
+			//分割若干子串
+			s.assign(str, i, k);
+
+			int cnt = 0;
+			string temp;
+			//重复查找，查找符合子串的数量
+			for(int j = 0; j < len - k; j++) {  
+				temp.assign(str, j, k);
+				if(s == temp) {
+					cnt++;
+				}
+			}
+
+			if(Maxcnt < cnt) {
+				Maxcnt = cnt;
+				ans = s;
+			}
+		}
+	}
+
+	cout << ans;
+	return 0;
+}
--- 蓝桥杯/算法训练/算法训练_字符统计（最长子串统计）.cpp
+++ 蓝桥杯/算法训练/算法训练_字符统计（最长子串统计）.cpp
@@ -4,6 +4,7 @@
 #include <cstring>
 #include <stack>
 #include <list>
+#include <map>
 #include <vector>
 #include <algorithm>
 using namespace std;
@@ -11,6 +12,15 @@ using namespace std;
 //给定一个长度为n的字符串S，还有一个数字L，
 //统计长度大于等于L的出现次数最多的子串（不同的出现可以相交），
 //如果有多个，输出最长的，如果仍然有多个，输出第一次出现最早的。
+
+static int L;
+
+bool comp(const pair<string,int> &a
+          , const  pair<string,int> &b  ) {
+
+	return 	a.second > b.second;
+}
+
 
 int main() {
 
@@ -21,20 +31,47 @@ int main() {
 //	string S;
 //	cin>>S;
 
-	string S = "desktop";
-	int L = 4;
+	string S = "bbaabbaaaaa";
+	L = 4;
+
+	string newStr;
 
 
+	//切割子串
+	int max = 0;
+	for(int i = 0 ; i <= S.length()-L+1; ++i) {
+		for(int j = S.length()-1 ; j >= i+L-1; --j) {
+			string str;
+			str.assign(S,i,j-i+1);
+//			cout<<str<<endl;
 
-	for(int i = L-1 ; i < S.length(); ++i) {
-		string str;
-		str.assign(S,i,str.length()-1);
-		cout<<str<<endl;
+
+			int cnt = 0;
+			//重复进行一次搜索
+			for(int k = S.length()-1 ; k >= i+L-1; --k) {
+				if(k != i) {
+					string strTwo;
+					strTwo.assign(S,i,j-i+1);
+					if(strTwo == str) {
+						cnt++;
+						cout<<"cnt = "<<cnt<<endl;
+					}
+				}
+
+			}
+
+			if(cnt > max){
+				max = cnt;
+				newStr = str;
+			}
+
+
+		}
 
 	}
 
 
+	cout<<newStr<<endl;
 
 
 }
-
