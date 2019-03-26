76147f25b79cdb1bf2905de571fce6dd1857595f
TimVan <877020296@qq.com>
2019-03-26 20:54:38 +0800

    最大子列和

---
+++ Java/src/com/timvan/算法训练/数据结构/第一讲/练习_1_3最大子列和.java
@@ -0,0 +1,16 @@
+package com.timvan.算法训练.数据结构.第一讲;
+
+/**
+ * 1.3 应用实例：最大子列和问题（3节共20:02）
+ * https://mooc.study.163.com/learn/1000033001?tid=2402970002#/learn/content?type=detail&id=2403307439&cid=2403323313
+ *
+ * @author TimVan on 2019/3/26
+ */
+public class 练习_1_3最大子列和 {
+    public static void main(String[] args) {
+        //输入数组
+        int N = 7;
+        int[] arr = {2,6,3,9,8,5,1};
+
+    }
+}
