94269afd49377868ee55c787dfd41c220305e2ec
TimVan <877020296@qq.com>
2019-04-09 15:14:37 +0800

    迷宫问题

--- Java/src/com/timvan/技巧/迷宫问题poj3984.java
+++ Java/src/com/timvan/技巧/迷宫问题poj3984.java
@@ -1,5 +1,6 @@
 package com.timvan.技巧;
 
+import java.util.Queue;
 import java.util.Scanner;
 
 /**
@@ -15,6 +16,74 @@ public class 迷宫问题poj3984 {
 //    Output
 //    左上角到右下角的最短路径，格式如样例所示。
 
+
+
+    static boolean isw[][] = new boolean[5][5];
+    static int a[][] = new int[5][5];
+    static int dx[] = {0,1,0,-1};
+    static int dy[] = {1,0,-1,0};
+
+    class Node{
+        int x;
+        int y;
+        int s;
+        short l[] = new short[30];
+    };
+
+    static private boolean judge(int x,int y)
+    {
+        if(x<0 || x>=5 || y<0 || y>=5){
+            return true;
+        }
+
+        if(isw[x][y]){
+            return true;
+        }
+
+        if(a[x][y]==1){
+            return true;
+        }
+
+        return false;
+    }
+
+     private Node bfs()
+    {
+        Queue<Node> q;
+        Node cur = new Node() ,next = new Node();
+        cur.x = 0;
+        cur.y = 0;
+        cur.s = 0;
+        isw[cur.x][cur.y] = true;
+        q.push(cur);
+        while(!q.isEmpty()){
+            cur = q.peek();
+            q.poll();
+            if(cur.x==4 && cur.y==4){
+                return cur;
+            }
+            int i,nx,ny;
+            for(i=0;i<4;i++){
+                nx = cur.x + dx[i];
+                ny = cur.y + dy[i];
+                if(judge(nx,ny)){
+                    continue;
+                }
+                //可以走
+                next = cur;
+                next.x = nx;
+                next.y = ny;
+                next.s = cur.s + 1;
+                next.l[cur.s] = (short) i;
+                q.add(next);
+            }
+        }
+        return cur;
+    }
+
+
+
+
     public static void main(String[] args) {
         //定义数组长度
         final int N = 5;
@@ -28,7 +97,15 @@ public class 迷宫问题poj3984 {
         }
 
         //进行BFS或者DFS
-
+        Node ans = new 迷宫问题poj3984().bfs();
+        int x,y;
+        x = 0;
+        y = 0;
+        for(int i=0 ; i<=ans.s ;i++){
+            System.out.println("("+x+", "+y+")\n");
+            x+=dx[ans.l[i]];
+            y+=dy[ans.l[i]];
+        }
 
     }
 }
