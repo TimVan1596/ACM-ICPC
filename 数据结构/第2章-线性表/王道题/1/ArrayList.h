86f84bc77713128e7470d68fee1c5c054c90fa8d
TimVan <877020296@qq.com>
2019-07-05 18:35:43 +0800

    线性表

--- 数据结构/第2章-线性表/王道题/1/ArrayList.h
+++ 数据结构/第2章-线性表/王道题/1/ArrayList.h
@@ -5,28 +5,75 @@
 using namespace std;
 
 template<typename T>
-class ArrayList{
+class ArrayList {
 	private:
 		//最大长度
 		int maxLength;
 		//存储数组
 		T *list;
 
+
 	public:
 		ArrayList(int _maxLength);
+		~ArrayList() {
+			cout<<"析构函数"<<endl;
+			delete []list;
+		}
 
+		// 查找
+		bool select (int i, T& element) const;
+		//插入
+		bool insert (int i, T element);
 
+		int getMaxLength() const {
+			return maxLength;
+		}
 };
 
 
 #endif
 
 template<typename T>
-ArrayList<T>::ArrayList(int _maxLength){
-//ArrayList::ArrayList(int _maxLength){
-
-	this.maxLength = _maxLength;
-	list = new T[maxLength];
+ArrayList<T>::ArrayList(int _maxLength) {
+	this->maxLength = _maxLength;
+	list = new T[maxLength]();
 	cout<<"构造函数"<<endl;
+}
+
+template<typename T>
+bool ArrayList<T>::select (int index, T& element) const {
+	if(checkIndexOutOfBound(index)) {
+		return false;
+	}
+	
+	element = list[index];
+	
+	return true;
+}
+
+template<typename T>
+bool ArrayList<T>::insert (int index, T element) {
+
+	if(checkIndexOutOfBound(index)) {
+		return false;
+	}
+
+
+	for(int i = maxLength-1 ; i >index ;  --i) {
+		list[i] = list[i-1];
+	}
+
+	list[index] = element;
+
+	return true;
+}
+
+
+static bool checkIndexOutOfBound(int index) {
+	if(index<0 || index >= maxLength) {
+		cout<<"--- i is out of bound ---"<<endl;
+		return false;
+	}
+	return true;
 }
 
--- 数据结构/第2章-线性表/王道题/1/main.cpp
+++ 数据结构/第2章-线性表/王道题/1/main.cpp
@@ -2,20 +2,36 @@
 #include <vector>
 
 #include "ArrayList.h"
-
 using namespace std;
 
-
-
-/* run this program using the console pauser or add your own getch,
- system("pause") or input loop */
+static void selectAll (ArrayList<int> list);
 
 int main(int argc, char** argv) {
 	
-	ArrayList<int> list(3);
+	ArrayList<int> list(10);
+	
+	int element = 123;
+	list.insert(0,element);
+	selectAll(list);
 	
 	//ArrayList list(3);
 
-	
 	return 0;
+}
+
+
+// 查找所有的element
+static void selectAll (ArrayList<int> list) {
+	cout<<"ArrayList={"<<endl;
+	for(int i = 0 ; i < list.getMaxLength();++i ){
+		int element  = 234;
+		if(list.select(i,element)){
+			cout<<i<<"->"<<element<<endl;
+		}else{
+			cout<<i<<"->"<<"Out of Bound"<<endl;
+		}
+		
+		
+	}
+	cout<<"}"<<endl;
 }
