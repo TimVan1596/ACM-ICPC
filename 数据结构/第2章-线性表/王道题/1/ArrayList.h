745d3791f4dd6557c0d557c4460bf2eab40c82df
TimVan <877020296@qq.com>
2019-07-07 12:24:29 +0800

    完成顺序表

--- 数据结构/第2章-线性表/王道题/1/ArrayList.h
+++ 数据结构/第2章-线性表/王道题/1/ArrayList.h
@@ -11,6 +11,9 @@ class ArrayList {
 		int maxLength;
 		//存储数组
 		T *list;
+		bool checkIndexOutOfBound(int index) const;
+		//当前长度
+		int currLength;
 
 
 	public:
@@ -20,13 +23,20 @@ class ArrayList {
 			delete []list;
 		}
 
-		// 查找
+		//查找
 		bool select (int i, T& element) const;
 		//插入
 		bool insert (int i, T element);
+		//删除
+		bool deleteByIndex (int i, T &element);
+		//更新
+		bool update (int i, T element);
 
 		int getMaxLength() const {
 			return maxLength;
+		}
+		int getCurrLength() const {
+			return currLength;
 		}
 };
 
@@ -37,41 +47,85 @@ template<typename T>
 ArrayList<T>::ArrayList(int _maxLength) {
 	this->maxLength = _maxLength;
 	list = new T[maxLength]();
-	cout<<"构造函数"<<endl;
+	currLength = 0;
 }
 
 template<typename T>
 bool ArrayList<T>::select (int index, T& element) const {
-	if(checkIndexOutOfBound(index)) {
+	if(!checkIndexOutOfBound(index)) {
 		return false;
 	}
-	
+
 	element = list[index];
-	
+
 	return true;
 }
 
 template<typename T>
 bool ArrayList<T>::insert (int index, T element) {
 
-	if(checkIndexOutOfBound(index)) {
+//	cout<<"[DEBUG]=>{"<<endl;
+//	cout<<"\t"<<"maxLength:"<<maxLength<<endl;
+//	cout<<"\t"<<"currLength:"<<currLength<<endl;
+//	cout<<"\t"<<"index:"<<index<<endl;
+//	cout<<"\t"<<"element:"<<element<<endl;
+//	cout<<"}"<<endl;
+
+	if(!checkIndexOutOfBound(index)) {
 		return false;
 	}
 
 
-	for(int i = maxLength-1 ; i >index ;  --i) {
+
+	for(int i = currLength-1 ; i >index ;  --i) {
 		list[i] = list[i-1];
 	}
 
 	list[index] = element;
+	currLength++;
+	return true;
+}
+
+
+template<typename T>
+bool ArrayList<T>::deleteByIndex (int index, T& element) {
+
+
+	if(!checkIndexOutOfBound(index)) {
+		return false;
+	}
+
+
+	element = list[index];
+	for(int i = index ; i < currLength-1 ;  ++i) {
+		list[i] = list[i+1];
+	}
+
+
+	currLength--;
+	return true;
+}
+
+template<typename T>
+bool ArrayList<T>::update (int index, T element) {
+	if(!checkIndexOutOfBound(index)) {
+		return false;
+	}
+
+	list[index]=element;
 
 	return true;
 }
 
 
-static bool checkIndexOutOfBound(int index) {
-	if(index<0 || index >= maxLength) {
-		cout<<"--- i is out of bound ---"<<endl;
+template<typename T>
+bool ArrayList<T>::checkIndexOutOfBound(int index) const {
+	if(index<0 || index > currLength) {
+		cout<<"[ERROR]=> i is out of bound"<<endl;
+		return false;
+	}
+	if(index > maxLength-1) {
+		cout<<"[ERROR]=> ArrayList space is not enough "<<endl;
 		return false;
 	}
 	return true;
--- 数据结构/第2章-线性表/王道题/1/main.cpp
+++ 数据结构/第2章-线性表/王道题/1/main.cpp
@@ -7,13 +7,23 @@ using namespace std;
 static void selectAll (ArrayList<int> list);
 
 int main(int argc, char** argv) {
-	
+
 	ArrayList<int> list(10);
-	
-	int element = 123;
-	list.insert(0,element);
+
+	for(int i = 0 ; i < 5 ; ++i) {
+		list.insert(i,(i+1)*(i+1));
+	}
 	selectAll(list);
-	
+
+	int deleteElement  = 0;
+	list.deleteByIndex(2,deleteElement);
+	cout<<"deleteElement="<<deleteElement<<endl;
+
+	selectAll(list);
+
+	list.update(2,233);
+	selectAll(list);
+
 	//ArrayList list(3);
 
 	return 0;
@@ -23,15 +33,13 @@ int main(int argc, char** argv) {
 // 查找所有的element
 static void selectAll (ArrayList<int> list) {
 	cout<<"ArrayList={"<<endl;
-	for(int i = 0 ; i < list.getMaxLength();++i ){
-		int element  = 234;
-		if(list.select(i,element)){
+	for(int i = 0 ; i < list.getCurrLength(); ++i ) {
+		int element  = 0;
+		if(list.select(i,element)) {
 			cout<<i<<"->"<<element<<endl;
-		}else{
+		} else {
 			cout<<i<<"->"<<"Out of Bound"<<endl;
 		}
-		
-		
 	}
 	cout<<"}"<<endl;
 }
