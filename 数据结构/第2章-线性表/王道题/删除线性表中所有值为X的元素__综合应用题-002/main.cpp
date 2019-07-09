d254a80811a3d150062b2a98f4bc1a7036120a1c
TimVan <877020296@qq.com>
2019-07-09 12:13:27 +0800

    删除线性表中所有值为X的元素

--- 数据结构/第2章-线性表/王道题/删除线性表中所有值为X的元素__综合应用题-002/main.cpp
+++ 数据结构/第2章-线性表/王道题/删除线性表中所有值为X的元素__综合应用题-002/main.cpp
@@ -4,41 +4,40 @@
 using namespace std;
 
 static void selectAll (ArrayList<int> list);
-static void reverseList (ArrayList<int> &list);
-
-//设计一个高效算法，将顺序表的所有元素逆置，
-//要求算法的空间复杂度为O(1).
+void deleteValue (ArrayList<int> &list , int value);
+//长度为n的顺序表L，编写一个时间复杂度为O(n),
+//空间复杂度为O(1)的算法，
+//该算法删除线性表中所有值为X的元素
 int main(int argc, char** argv) {
+
 	ArrayList<int> list(10);
 	const int LEN = 7;
-	int arr[LEN] = {14,6,20,9,7,11,1};
-	for(int i = 0; i < 2 ; ++i ) {
+	int arr[LEN] = {14,6,14,9,7,11,14};
+	for(int i = 0; i < LEN ; ++i ) {
 		list.insert(i,arr[i]);
 	}
 	selectAll(list);
-	
-	reverseList(list);
+
+	deleteValue(list,14);
 	selectAll(list);
 
 	return 0;
 }
 
 
-// 将顺序表的所有元素逆置
-static void reverseList (ArrayList<int> &list) {
-	int CurrLength = list.getCurrLength();
-	if(CurrLength < 2) {
-		return;
-	}
-	int N = CurrLength/2;
-
-	for(int i = 0; i < N ; ++i ) {
-		int a  = 0;
-		list.select(i,a);
-		int b  = 0;
-		list.select(CurrLength-i-1,b);
-		list.update(i,b);
-		list.update(CurrLength-i-1,a);
+// 删除线性表中所有值为X的元素
+void deleteValue (ArrayList<int> &list , int value) {
+	for(int i = 0 ; i < list.getCurrLength(); ++i ) {
+		int element  = 0;
+		if(list.select(i,element)) {
+			if(element == value) {
+				list.deleteByIndex(i,element);
+				i--;
+			}
+		} else {
+			cout<<i<<"->"<<"Out of Bound"<<endl;
+			return;
+		}
 	}
 }
 
@@ -55,6 +54,4 @@ static void selectAll (ArrayList<int> list) {
 		}
 	}
 	cout<<"}"<<endl;
-
-
 }
