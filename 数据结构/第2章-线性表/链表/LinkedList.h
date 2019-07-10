33146ea3241269b3357782d4e92a4ac2d0be6526
TimVan <877020296@qq.com>
2019-07-10 20:14:10 +0800

    链表初步

---
+++ 数据结构/第2章-线性表/链表/LinkedList.h
@@ -0,0 +1,19 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+#include <iostream>
+#include "LinearList.h"
+using namespace std;
+
+template<typename T>
+class LinkedList {
+	public:
+		LinkedList();
+		
+}; 
+#endif
+
+
+template<typename T>
+LinkedList<T>::LinkedList(){
+}
---
+++ 数据结构/第2章-线性表/链表/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "LinkedList.h"
+using namespace std;
+
+//¡¥±Ì≤‚ ‘ 
+int main(int argc, char** argv) {
+	
+	LinkedList<int> list;
+	list.print();		
+	return 0;
+}
