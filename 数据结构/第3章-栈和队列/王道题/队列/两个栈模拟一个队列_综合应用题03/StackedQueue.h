2e580e14dcaeb4c69e82eda0d7965b0f08573d33
TimVan <877020296@qq.com>
2019-08-31 14:07:13 +0800

    修复-两个栈模拟一个队列

--- 数据结构/第3章-栈和队列/王道题/队列/两个栈模拟一个队列_综合应用题03/StackedQueue.h
+++ 数据结构/第3章-栈和队列/王道题/队列/两个栈模拟一个队列_综合应用题03/StackedQueue.h
@@ -11,9 +11,26 @@ class StackedQueue {
 		X_SeqStack<T> *S1;
 		X_SeqStack<T> *S2;
 
-		//QueueFull; 判断队列是否为满，S1为满
+		//QueueFull; 判断队列是否为满
+		//原理：S1为空，或者S1为满但S2为空时，队列为满
 		bool QueueFull() {
-			return (X_SeqStack<T>::StackOverflow(*S1));
+
+			if(!(X_SeqStack<T>::StackOverflow(*S1))) {
+				return false;
+			}
+
+			//!注意，这里隐含条件，S1为满
+			if((X_SeqStack<T>::StackEmpty(*S2))) {
+
+				while(!X_SeqStack<T>::StackEmpty(*S1)) {
+					T element;
+					X_SeqStack<T>::Pop(*S1,element);
+					X_SeqStack<T>::Push(*S2,element);
+				}
+				return false;
+			}
+
+			return true;
 		}
 	public:
 		StackedQueue(int size) {
@@ -54,13 +71,11 @@ class StackedQueue {
 			return true;
 		}
 		//QueueEmpty; 判断队列是否为空
-		//原理判断S1是否为空 
+		//原理判断S1和S2是否为空
 		bool QueueEmpty() {
 			return (X_SeqStack<T>::StackEmpty(*S1))
 			       && (X_SeqStack<T>::StackEmpty(*S2));
 		}
-
-
 };
 
 #endif
