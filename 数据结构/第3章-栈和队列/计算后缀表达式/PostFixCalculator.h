f99b789852083516c51d42053a560a676bcb6450
TimVan <877020296@qq.com>
2019-09-05 17:44:54 +0800

    中缀转后缀表达式

--- 数据结构/第3章-栈和队列/计算后缀表达式/PostFixCalculator.h
+++ 数据结构/第3章-栈和队列/计算后缀表达式/PostFixCalculator.h
@@ -12,26 +12,11 @@ class PostFixCalculator {
 			stack = new SeqStack<double>(size);
 		}
 
-		double run(string str) {
-			double ret;
+		//执行后缀表达式计算，并返回结果
+		double run(string str);
 
-			for(int i = 0; i<str.size() ; ++i) {
-				char ch = str.at(i);
-				double num = 0;
-				if(ch >= '0' && ch <= '9') {
-					num = ch-'0';
-				} else {
-					num = doOperator(ch);
-				}
-				pushOperand(num);
-				cout<<"pushOperand("<<num<<");"<<endl;
-
-			}
-			stack->pop(ret);
-			return ret;
-		}
-
-
+		//中缀表达式转后缀表达式
+		static string InFixToPostFix(string str);
 	private:
 		//操作数的栈
 		SeqStack<double> *stack;
@@ -48,43 +33,90 @@ class PostFixCalculator {
 		}
 
 		//进行操作数与操作符的计算
-		double doOperator(char ch) {
-			double ret;
-			double num1 ,num2;
-			getOperands(num1,num2);
-
-
-			switch(ch) {
-				case '+': {
-					ret = num1+num2;
-					break;
-				}
-
-				case '-': {
-					ret = num2-num1;
-					break;
-				}
-
-				case '*': {
-					ret = num2*num1;
-					break;
-				}
-
-				case '/': {
-					ret = num2/num1;
-					break;
-				}
-
-				default: {
-					cout<<"doOperator:[ERROR] Operator is Illegal"<<endl;
-					break;
-				}
-			}
-
-			cout<<"doOperator:("<<num1<<ch<<num2<<") = "<<ret<<endl;
-			return ret;
-		}
-
+		double doOperator(char ch);
 };
 
+double PostFixCalculator::run(string str)  {
+	double ret;
+
+	for(unsigned int i = 0; i<str.size() ; ++i) {
+		char ch = str.at(i);
+		double num = 0;
+		if(ch >= '0' && ch <= '9') {
+			num = ch-'0';
+		} else {
+			num = doOperator(ch);
+		}
+		pushOperand(num);
+		//cout<<"pushOperand("<<num<<");"<<endl;
+	}
+	stack->pop(ret);
+	return ret;
+}
+
+double PostFixCalculator::doOperator(char ch) {
+	double ret;
+	double num1 ,num2;
+	getOperands(num1,num2);
+
+
+	switch(ch) {
+		case '+': {
+			ret = num1+num2;
+			break;
+		}
+
+		case '-': {
+			ret = num2-num1;
+			break;
+		}
+
+		case '*': {
+			ret = num2*num1;
+			break;
+		}
+
+		case '/': {
+			ret = num2/num1;
+			break;
+		}
+
+		default: {
+			cout<<"doOperator:[ERROR] Operator is Illegal"<<endl;
+			break;
+		}
+	}
+
+	//cout<<"doOperator:("<<num1<<ch<<num2<<") = "<<ret<<endl;
+	return ret;
+}
+
+string PostFixCalculator::InFixToPostFix(string str) {
+	string ret = "";
+	int cache_num = 0;
+
+	cout<<"--------- InFixToPostFix Start --------"<<endl;
+	cout<<"str = "<<str<<endl;
+	int arrIndex = 0;
+	for(unsigned int i=0; i<str.size(); ++i) {
+		char c = str.at(i);
+		if((c-'0')>=0 && (c-'0')<=9) {
+			cache_num = cache_num*10 + (c-'0');
+		} else {
+			if(cache_num != 0) {
+				cout<<++arrIndex<<":"<<cache_num<<endl;
+				cache_num = 0;
+			}
+			cout<<++arrIndex<<":"<<c<<endl;
+		}
+
+	}
+	if(cache_num != 0) {
+		cout<<++arrIndex<<":"<<cache_num<<endl;
+		cache_num = 0;
+	}
+	cout<<"--------- InFixToPostFix End --------"<<endl;
+
+	return ret;
+}
 #endif
--- 数据结构/第3章-栈和队列/计算后缀表达式/main.cpp
+++ 数据结构/第3章-栈和队列/计算后缀表达式/main.cpp
@@ -4,12 +4,18 @@ using namespace std;
 
 int main(int argc, char** argv) {
 
+	const int LEN = 20;
 	string str = "642-/32*+";
-	PostFixCalculator cal(str.size());
+	PostFixCalculator cal(LEN);
 	cout<<"642-/32*+  ="<<cal.run(str)<<endl;
 	str = "123+4*+5-";
 	cout<<str<<"  ="<<cal.run(str)<<endl;
 	str = "2963/+5-*4+";
 	cout<<str<<"  ="<<cal.run(str)<<endl;
+	str = "90/3+(30/2-1)*3";
+	PostFixCalculator::InFixToPostFix(str);
+	str = "24*19-34/17+(128+35*3)";
+	PostFixCalculator::InFixToPostFix(str);
+
 	return 0;
 }
