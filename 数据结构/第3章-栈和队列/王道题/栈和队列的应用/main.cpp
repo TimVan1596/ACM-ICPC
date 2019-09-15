c09b508e2532fcb915533dd1ef91286f01a4d508
TimVan <877020296@qq.com>
2019-09-15 13:35:17 +0800

    完成判别表达式括号匹配

--- 数据结构/第3章-栈和队列/王道题/栈和队列的应用/main.cpp
+++ 数据结构/第3章-栈和队列/王道题/栈和队列的应用/main.cpp
@@ -14,19 +14,23 @@ int main(int argc, char** argv) {
 
 	string str = "{[3*4-(6*1.1)]/[3+0]}/{[3-2]+[2*2]}+{[(3.14)]}\0";
 	cout<<"{\n Expression = "<<str<<endl;
-	cout<<"Bracket is Correct = "<<isBracketCorrect(str)<<"}"<<endl<<endl;
+	cout<<"Bracket is Correct = "<<isBracketCorrect(str)<<endl<<"}"<<endl<<endl;
 
 	str = "{([])}";
 	cout<<"{\n Expression = "<<str<<endl;
-	cout<<"Bracket is Correct = "<<isBracketCorrect(str)<<"}"<<endl<<endl;
+	cout<<"Bracket is Correct = "<<isBracketCorrect(str)<<endl<<"}"<<endl<<endl;
 
 	str = "{([)}";
 	cout<<"{\n Expression = "<<str<<endl;
-	cout<<"Bracket is Correct = "<<isBracketCorrect(str)<<"}"<<endl<<endl;
+	cout<<"Bracket is Correct = "<<isBracketCorrect(str)<<endl<<"}"<<endl<<endl;
 
 	str = "3*{3-(2019/[33)}/8";
 	cout<<"{\n Expression = "<<str<<endl;
-	cout<<"Bracket is Correct = "<<isBracketCorrect(str)<<"}"<<endl<<endl;
+	cout<<"Bracket is Correct = "<<isBracketCorrect(str)<<endl<<"}"<<endl<<endl;
+
+	str = "3*{3-(2019/[33])}/8";
+	cout<<"{\n Expression = "<<str<<endl;
+	cout<<"Bracket is Correct = "<<isBracketCorrect(str)<<endl<<"}"<<endl<<endl;
 
 	return 0;
 }
@@ -40,12 +44,29 @@ const bool isBracketCorrect(string str) {
 
 	for(int i = 0 ; i < len ; ++i) {
 		char ch = str.at(i);
+
 		if(ch == '{' || ch == '(' || ch == '[') {
 			bracketStack.push(ch);
-		} else if(ch == '}' || ch == ')' || ch == ']') {
+		} else if(ch == '}') {
 			char previousChar;
 			bracketStack.top(previousChar);
-			if(previousChar == ch) {
+			if(previousChar == '{' ) {
+				bracketStack.pop(previousChar);
+			} else {
+				isCorrect = false;
+			}
+		} else if(ch == ')') {
+			char previousChar;
+			bracketStack.top(previousChar);
+			if(previousChar == '(') {
+				bracketStack.pop(previousChar);
+			} else {
+				isCorrect = false;
+			}
+		} else if(ch == ']') {
+			char previousChar;
+			bracketStack.top(previousChar);
+			if(previousChar == '[') {
 				bracketStack.pop(previousChar);
 			} else {
 				isCorrect = false;
@@ -57,5 +78,5 @@ const bool isBracketCorrect(string str) {
 		isCorrect = false;
 	}
 
-	return true;
+	return isCorrect;
 }
