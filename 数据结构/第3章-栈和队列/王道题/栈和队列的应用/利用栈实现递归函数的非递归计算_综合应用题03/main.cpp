7a58445b8edefab0c3f1f114bbf44aec1ff6fbef
TimVan <877020296@qq.com>
2019-09-17 11:57:46 +0800

    递归计算

--- 数据结构/第3章-栈和队列/王道题/栈和队列的应用/利用栈实现递归函数的非递归计算_综合应用题03/main.cpp
+++ 数据结构/第3章-栈和队列/王道题/栈和队列的应用/利用栈实现递归函数的非递归计算_综合应用题03/main.cpp
@@ -1,31 +1,35 @@
 #include <iostream>
 
 #include "SeqStack.h"
-using namespace std; 
+using namespace std;
 
 double reCurCal(double x,int n);
 
 /* 利用栈实现递归函数的非递归计算 */
 int main(int argc, char** argv) {
 	double x = 1;
-	int n = 3;	
+	int n = 2;
 	cout<<"x = "<<x<<"，n="<<n<<endl;
-	cout<<"reCurCal = "<<reCurCal(x,n)<<endl;
-	
+	cout<<"reCurCal = "<<reCurCal(x,n)<<endl<<endl;
+
+	x = 1;
+	n = 3;
+	cout<<"x = "<<x<<"，n="<<n<<endl;
+	cout<<"reCurCal = "<<reCurCal(x,n)<<endl<<endl;
 	return 0;
 }
 
-//未知数x 和 阶数n 
-double reCurCal(double x,int n){
+//未知数x 和 阶数n
+double reCurCal(double x,int n) {
 	double ret;
-	
-	if(n == 0){
+
+	if(n == 0) {
 		ret = 1;
-	}else if(n == 1){
+	} else if(n == 1) {
 		ret = 2*x;
-	}else{
+	} else {
 		ret = 2*x*reCurCal(x,n-1)-2*(n-1)*reCurCal(x,n-2);
 	}
-	
+
 	return ret;
 }
