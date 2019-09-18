022720707f8951ade4a9194878d61c17979d4a82
TimVan <877020296@qq.com>
2019-09-18 11:51:11 +0800

    非递归计算

--- 数据结构/第3章-栈和队列/王道题/栈和队列的应用/利用栈实现递归函数的非递归计算_综合应用题03/main.cpp
+++ 数据结构/第3章-栈和队列/王道题/栈和队列的应用/利用栈实现递归函数的非递归计算_综合应用题03/main.cpp
@@ -4,21 +4,29 @@
 using namespace std;
 
 double reCurCal(double x,int n);
+double noReCurCal(double x,int n);
 
 /* 利用栈实现递归函数的非递归计算 */
 int main(int argc, char** argv) {
 	double x = 1;
 	int n = 2;
-	cout<<"x = "<<x<<"，n="<<n<<endl;
-	cout<<"reCurCal = "<<reCurCal(x,n)<<endl<<endl;
 
 	x = 1;
 	n = 3;
-	cout<<"x = "<<x<<"，n="<<n<<endl;
-	cout<<"reCurCal = "<<reCurCal(x,n)<<endl<<endl;
+
+	for(int i = 1 ; i <= 4 ; ++i) {
+		if(reCurCal(i*i,i+i) == noReCurCal(i*i,i+i)) {
+			cout<<i<<": x="<<i*i<<",n="<<i+i<<"->相等"<<endl;
+		} else {
+			cout<<i<<": x="<<i*i<<",n="<<i+i<<"->!不相等!"<<endl;
+
+		}
+	}
+
 	return 0;
 }
 
+//递归方法
 //未知数x 和 阶数n
 double reCurCal(double x,int n) {
 	double ret;
@@ -32,4 +40,19 @@ double reCurCal(double x,int n) {
 	}
 
 	return ret;
+}
+
+//非递归方法
+double noReCurCal(double x,int n) {
+	double results[n] = {0};
+	for(int i = 0 ; i <= n ; ++i) {
+		if(i == 0) {
+			results[i] = 1;
+		} else if(i == 1) {
+			results[i] = x*2;
+		} else {
+			results[i] = 2*x*results[i-1]-2*(i-1)*results[i-2];
+		}
+	}
+	return results[n];
 }
