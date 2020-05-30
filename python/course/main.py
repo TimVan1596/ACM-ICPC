7d6820522d96dbc8763d1e3db9430879d6a4b461
TimVan <877020296@qq.com>
2020-05-30 15:55:09 +0800

    python初步

---
+++ python/course/main.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+# @Time:2020/5/3011:41
+# @Author:TimVan
+# @File:main.py
+# @Software:PyCharm
+
+# 单行注释
+print("hello world")
+
+'''
+  这个是多行注释
+  注意UTF-8的coding
+'''
+print("你们好")
