094e2b3e98db216e3f771cf25dbc6d2ef9f8e027
TimVan <877020296@qq.com>
2020-07-13 22:42:09 +0800

    输入和输出

--- python/course/main.py
+++ python/course/main.py
@@ -550,8 +550,39 @@ print(tup)
 #     except StopIteration:
 #         sys.exit()
 
-a = 10
-b = 20
-a, b = b, a + b
-print("a=%d" % a)
-print("b=%d" % b)
+# a = 10
+# b = 20
+# a, b = b, a + b
+# print("a=%d" % a)
+# print("b=%d" % b)
+
+
+# Python3 输入和输出
+# 输出格式美化
+# Python两种输出值的方式: 表达式语句和 print() 函数。
+# 第三种方式是使用文件对象的 write() 方法，标准输出文件可以用 sys.stdout 引用。
+# 如果你希望输出的形式更加多样，可以使用 str.format() 函数来格式化输出值。
+# 如果你希望将输出的值转成字符串，可以使用 repr() 或 str() 函数来实现。
+# str()： 函数返回一个用户易读的表达形式。
+# repr()： 产生一个解释器易读的表达形式。
+# 例如
+# >>> s = 'Hello, Runoob'
+# >>> str(s)
+# 'Hello, Runoob'
+# >>> repr(s)
+# "'Hello, Runoob'"
+# >>> str(1/7)
+# '0.14285714285714285'
+# >>> x = 10 * 3.25
+# >>> y = 200 * 200
+# >>> s = 'x 的值为： ' + repr(x) + ',  y 的值为：' + repr(y) + '...'
+# >>> print(s)
+# x 的值为： 32.5,  y 的值为：40000...
+# >>> #  repr() 函数可以转义字符串中的特殊字符
+# ... hello = 'hello, runoob\n'
+# >>> hellos = repr(hello)
+# >>> print(hellos)
+# 'hello, runoob\n'
+# >>> # repr() 的参数可以是 Python 的任何对象
+# ... repr((x, y, ('Google', 'Runoob')))
+# "(32.5, 40000, ('Google', 'Runoob'))"
