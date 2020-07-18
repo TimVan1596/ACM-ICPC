9d3d3d557fbe6a2f955fb5ab0f834124557ffc4a
TimVan <877020296@qq.com>
2020-07-18 13:57:26 +0800

    Python3 OS 文件/目录方法

--- python/course/main.py
+++ python/course/main.py
@@ -948,15 +948,81 @@ print(tup)
 # C> print('Runoob!')
 # Runoob!
 # C>
-import sys
-from os import path
+# import sys
+# from os import path
+#
+# print(sys.path[0])
+# print(path)
+#
+# f = open("test\main.txt", "r")
+# # f.writelines(['大家好\n', '我是Python3\n', '根地址:' + sys.path[0]+'\n'])
+# # str = f.read()
+# str = f.readlines()
+# print('str=%s'%str)
+# f.close()
 
-print(sys.path[0])
-print(path)
+# Python3 os文件和目录方法
+# Python3 os.access() 方法
+#
+# 概述
+# os.access() 方法使用当前的uid/gid尝试访问路径。
+# 大部分操作使用有效的 uid/gid, 因此运行环境可以在 suid/sgid 环境尝试。
+# GID为GroupId，即组ID，用来标识用户组的唯一标识符
+# UID为UserId，即用户ID，用来标识每个用户的唯一标示符
+# 语法
+# access()方法语法格式如下：
+#
+# os.access(path, mode);
+# 参数
+# path -- 要用来检测是否有访问权限的路径。
+#
+# mode -- mode为F_OK，测试存在的路径，
+# 或者它可以是包含R_OK, W_OK和X_OK或者R_OK, W_OK和X_OK其中之一或者更多。
+#
+# os.F_OK: 作为access()的mode参数，测试path是否存在。
+# os.R_OK: 包含在access()的mode参数中 ， 测试path是否可读。
+# os.W_OK 包含在access()的mode参数中 ， 测试path是否可写。
+# os.X_OK 包含在access()的mode参数中 ，测试path是否可执行。
+# 返回值
+# 如果允许访问返回 True , 否则返回False。
+#
+# 实例
+# 以下实例演示了 access() 方法的使用：
+#
+# #!/usr/bin/python3
+#
+# import os, sys
+#
+# # 假定 /tmp/foo.txt 文件存在，并有读写权限
+#
+# ret = os.access("/tmp/foo.txt", os.F_OK)
+# print ("F_OK - 返回值 %s"% ret)
+#
+# ret = os.access("/tmp/foo.txt", os.R_OK)
+# print ("R_OK - 返回值 %s"% ret)
+#
+# ret = os.access("/tmp/foo.txt", os.W_OK)
+# print ("W_OK - 返回值 %s"% ret)
+#
+# ret = os.access("/tmp/foo.txt", os.X_OK)
+# print ("X_OK - 返回值 %s"% ret)
+# 执行以上程序输出结果为：
+#
+# F_OK - 返回值 True
+# R_OK - 返回值 True
+# W_OK - 返回值 True
+# X_OK - 返回值 False
 
-f = open("test\main.txt", "r")
-# f.writelines(['大家好\n', '我是Python3\n', '根地址:' + sys.path[0]+'\n'])
-# str = f.read()
-str = f.readlines()
-print('str=%s'%str)
-f.close()
+import sys, os
+
+path = "test\copy.txt"
+ret = os.access(path, os.F_OK)
+print("{}的os.F_OK={}".format(path, ret))
+
+path = "test\\test.txt"
+ret = os.access(path, os.F_OK)
+print("{}的os.F_OK={}".format(path, ret))
+
+path = "test\copy.txt"
+ret = os.access(path, os.X_OK)
+print("{}的os.X_OK={}".format(path, ret))
