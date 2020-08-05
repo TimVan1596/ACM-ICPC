731c62de3fb25cc2b8f89903162d0dcf01b9b509
TimVan <877020296@qq.com>
2020-08-05 18:02:46 +0800

    test Urllib

--- python/douban/test/testUrllib.py
+++ python/douban/test/testUrllib.py
@@ -6,6 +6,44 @@
 
 import urllib.request
 
-baseUrl = "https://www.timvanx.com/"
-response = urllib.request.urlopen(baseUrl)
-print(response.read().decode("gbk"))
+# baseUrl = "https://www.timvanx.com/"
+#
+# # get方式请求
+# response = urllib.request.urlopen(baseUrl)
+# # 对获取到的网页源码进行utf-8解码
+# print(response.read().decode("utf-8"))
+
+# post方式请求
+# import urllib.parse
+# data = bytes(urllib.parse.urlencode({"id": 2}), encoding="utf-8")
+# postBaseUrl = "https://www.timvanx.com/php/getnewsitem.php"
+# # 超时处理
+# try:
+#     response = urllib.request.urlopen(postBaseUrl
+#                                       , data=data
+#                                       ,timeout=0.01)
+#     print(response.read().decode("utf-8"))
+# except urllib.error.URLError as e:
+#     print("结果超时:{0}".format(e))
+
+
+
+# import urllib.parse
+# data = bytes(urllib.parse.urlencode({"id": 2}), encoding="utf-8")
+# # postBaseUrl = "https://www.timvanx.com/php/getnewsitem.php"
+# postBaseUrl = "https://www.baidu.com"
+# response = urllib.request.urlopen(postBaseUrl
+#                                   , data=data
+#                                   ,timeout=30)
+# print(response.read().decode("utf-8"))
+# print(response.status)
+# print(response.getheaders())
+# print(response.getheader("Server"))
+
+# 对豆瓣进行爬取
+douBanUrl = "https://www.baidu.com"
+# User-Agent:
+# Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36
+headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36"}
+req = urllib.request.Request(url=douBanUrl,data=data
+                             ,headers=headers,method="get")
