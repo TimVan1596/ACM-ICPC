f6d3f0897a5959149ac575970217a8313679cde8
TimVan <877020296@qq.com>
2021-05-08 17:29:59 +0800

    先序遍历

--- python/course/leetcode/algorithm-pattern/二叉树/先序遍历.py
+++ python/course/leetcode/algorithm-pattern/二叉树/先序遍历.py
@@ -22,15 +22,38 @@ class BinaryTree:
     # 驱动函数
     def preOrderDerivativeDriver(self):
         self.preOrderDerivative(self.root)
+        print("")
 
     # 递归的先序遍历，中左右
     def preOrderDerivative(self, node):
         if node is not None:
-            print(node)
+            print(node, end=" ")
             self.preOrderDerivative(node.leftNode)
             self.preOrderDerivative(node.rightNode)
 
+    # 驱动函数
+    def preOrderDriver(self):
+        self.preOrder(self.root)
+        print("")
 
+    # 先序遍历，中左右
+    def preOrder(self, node: Node):
+        # 定义一个栈，先进后出
+        stack = []
+        point = node
+        while point is not None:
+            # 打印当前节点
+            print(point, end=" ")
+            # 右节点是否为空，非空放入栈
+            if point.rightNode is not None:
+                stack.append(point.rightNode)
+            # 左节点如非空则进入，若为空从栈中取顶值
+            if point.leftNode is not None:
+                point = point.leftNode
+            elif len(stack) > 0:
+                point = stack.pop()
+            else:
+                break
 
 
 #         A
@@ -49,4 +72,6 @@ myBinaryTree.root.leftNode.rightNode = ENode
 myBinaryTree.root.rightNode = CNode
 myBinaryTree.root.rightNode.leftNode = FNode
 myBinaryTree.root.rightNode.rightNode = GNode
+myBinaryTree.preOrderDerivativeDriver()
+print("-" * 10)
 myBinaryTree.preOrderDerivativeDriver()
