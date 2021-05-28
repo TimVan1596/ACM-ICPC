90e8e114eb2ac4b02c145d0f003bff110aa8c977
877020296@qq.com <ff520linda>
2021-05-28 22:13:39 +0800

    更新

--- python/DeepLearning/test.py
+++ python/DeepLearning/test.py
@@ -1,14 +1,23 @@
 import numpy as np
+import math
 
-# a=3*1的列矩阵
-a = np.random.randn(4, 3)
-# b=1*3的行矩阵
-b = np.random.randn(3, 2)
-print(a.shape)
-print(b.shape)
-print("np.dot(a, b)=")
-print(np.dot(a, b))
-aMatrix = np.mat(a)
-bMatrix = np.mat(b)
-print("矩阵相乘=")
-print(aMatrix * bMatrix)
+# # a=3*1的列矩阵
+# a = np.random.randn(4, 3)
+# # b=1*3的行矩阵
+# b = np.random.randn(3, 2)
+# print(a.shape)
+# print(b.shape)
+# print("np.dot(a, b)=")
+# print(np.dot(a, b))
+# aMatrix = np.mat(a)
+# bMatrix = np.mat(b)
+# print("矩阵相乘=")
+# print(aMatrix * bMatrix)
+
+oneA = (1 / (1 + math.exp(-9))) - 1
+oneB = (2 / (1 + math.exp(-12)))
+
+dw1 = [[
+    0.5*(oneA + oneB)
+], [ 0.5*(3 * oneA + 2 * oneB)]]
+print(dw1)
