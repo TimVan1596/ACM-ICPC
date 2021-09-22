e9c220c34c8d0666194f9b0a299925f1c7427b43
TimVan <877020296@qq.com>
2021-09-22 21:14:04 +0800

    week3-初步浅层神经网络

--- python/DeepLearning/week3/homework/test.py
+++ python/DeepLearning/week3/homework/test.py
@@ -1,12 +1,20 @@
 import numpy as np
+import numpy.random
+
+# 设置常量
+# HIDDEN_LAYER_NUM = 隐层的个数
+HIDDEN_LAYER_NUM = 4
 
 
 # 激活函数
-def activate():
-    pass
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
 
 
 if __name__ == '__main__':
+    # 设置随机种子
+    numpy.random.seed(20210922)
+
     # 初始化大写 X
     X = np.array([
         [20, 11],
@@ -17,23 +25,75 @@ if __name__ == '__main__':
     print(X)
     print()
 
+    # 初始化大写Y
+    Y = np.array([0, 1])
+
     # 初始化大写 W
-    W1 = np.array([
-        [1, 4, 7, 7],
-        [2, 5, 8, 6],
-        [3, 6, 9, 2],
-    ])
-    W1 = W1.T
+    W1 = np.random.uniform(-1, 1, size=(HIDDEN_LAYER_NUM, X.shape[0]))
     print('W1=')
     print(W1)
     print()
 
     # 初始化 B
     # b1 = np.random.randint(1, 1, size=(4, 2))
-    b1 = np.zeros((4, 2),dtype=int)
+    b1 = np.random.uniform(-1, 1, size=(HIDDEN_LAYER_NUM, X.shape[1]))
     print('b1=')
     print(b1)
     print()
 
     Z1 = W1.dot(X) + b1
+    print('Z1=')
     print(Z1)
+    print()
+
+    A1 = np.tanh(Z1)
+    print('A1=')
+    print(A1)
+    print()
+
+    W2 = np.random.uniform(-1, 1, size=(1, HIDDEN_LAYER_NUM))
+    print('W2=')
+    print(W2)
+    print()
+
+    b2 = np.random.uniform(-1, 1, size=(1, X.shape[1]))
+    print('b2=')
+    print(b2)
+    print()
+
+    Z2 = W2.dot(A1) + b2
+    print('Z2=')
+    print(Z2)
+    print()
+
+    A2 = sigmoid(Z2)
+    print('A2=')
+    print(A2)
+    print()
+
+    d_Z2 = A2 - Y
+    print('d_Z2=')
+    print(d_Z2)
+    print()
+
+    d_W2 = (1 / X.shape[1]) * (d_Z2.dot(A1.T))
+    print('d_W2=')
+    print(d_W2)
+    print()
+
+    d_b2 = (1 / X.shape[1]) * np.sum(d_Z2, axis=1, keepdims=True)
+    print('d_b2=')
+    print(d_b2)
+    print()
+
+    print('np.sum(d_Z2, axis=1, keepdims=True)=')
+    print(np.sum(d_Z2, axis=1, keepdims=True))
+    print()
+
+    print('np.sum(d_Z2, axis=1)=')
+    print(np.sum(d_Z2, axis=1))
+    print()
+
+    print('np.sum(d_Z2)=')
+    print(np.sum(d_Z2))
+    print()
