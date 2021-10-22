1f4ce47673ed52d63e66b171e901cfb0e5829107
TimVan <877020296@qq.com>
2021-10-22 20:50:17 +0800

    L2W1-初步优化网络

--- python/DeepLearning/lesson2/week1/init_utils.py
+++ python/DeepLearning/lesson2/week1/init_utils.py
@@ -16,9 +16,10 @@ def sigmoid(x):
     Return:
     s -- sigmoid(x)
     """
-    s = 1/(1+np.exp(-x))
+    s = 1 / (1 + np.exp(-x))
     return s
- 
+
+
 def relu(x):
     """
     Compute the relu of x
@@ -29,12 +30,12 @@ def relu(x):
     Return:
     s -- relu(x)
     """
-    s = np.maximum(0,x)
-    
+    s = np.maximum(0, x)
+
     return s
-    
+
+
 def compute_loss(a3, Y):
-    
     """
     Implement the loss function
     
@@ -45,13 +46,14 @@ def compute_loss(a3, Y):
     Returns:
     loss - value of the loss function
     """
-    
+
     m = Y.shape[1]
-    logprobs = np.multiply(-np.log(a3),Y) + np.multiply(-np.log(1 - a3), 1 - Y)
-    loss = 1./m * np.nansum(logprobs)
-    
+    logprobs = np.multiply(-np.log(a3), Y) + np.multiply(-np.log(1 - a3), 1 - Y)
+    loss = 1. / m * np.nansum(logprobs)
+
     return loss
-    
+
+
 def forward_propagation(X, parameters):
     """
     Implements the forward propagation (and computes the loss) presented in Figure 2.
@@ -70,7 +72,7 @@ def forward_propagation(X, parameters):
     Returns:
     loss -- the loss function (vanilla logistic loss)
     """
-        
+
     # retrieve parameters
     W1 = parameters["W1"]
     b1 = parameters["b1"]
@@ -78,7 +80,7 @@ def forward_propagation(X, parameters):
     b2 = parameters["b2"]
     W3 = parameters["W3"]
     b3 = parameters["b3"]
-    
+
     # LINEAR -> RELU -> LINEAR -> RELU -> LINEAR -> SIGMOID
     z1 = np.dot(W1, X) + b1
     a1 = relu(z1)
@@ -86,11 +88,12 @@ def forward_propagation(X, parameters):
     a2 = relu(z2)
     z3 = np.dot(W3, a2) + b3
     a3 = sigmoid(z3)
-    
+
     cache = (z1, a1, W1, b1, z2, a2, W2, b2, z3, a3, W3, b3)
-    
+
     return a3, cache
- 
+
+
 def backward_propagation(X, Y, cache):
     """
     Implement the backward propagation presented in figure 2.
@@ -105,27 +108,28 @@ def backward_propagation(X, Y, cache):
     """
     m = X.shape[1]
     (z1, a1, W1, b1, z2, a2, W2, b2, z3, a3, W3, b3) = cache
-    
-    dz3 = 1./m * (a3 - Y)
+
+    dz3 = 1. / m * (a3 - Y)
     dW3 = np.dot(dz3, a2.T)
-    db3 = np.sum(dz3, axis=1, keepdims = True)
-    
+    db3 = np.sum(dz3, axis=1, keepdims=True)
+
     da2 = np.dot(W3.T, dz3)
     dz2 = np.multiply(da2, np.int64(a2 > 0))
     dW2 = np.dot(dz2, a1.T)
-    db2 = np.sum(dz2, axis=1, keepdims = True)
-    
+    db2 = np.sum(dz2, axis=1, keepdims=True)
+
     da1 = np.dot(W2.T, dz2)
     dz1 = np.multiply(da1, np.int64(a1 > 0))
     dW1 = np.dot(dz1, X.T)
-    db1 = np.sum(dz1, axis=1, keepdims = True)
-    
+    db1 = np.sum(dz1, axis=1, keepdims=True)
+
     gradients = {"dz3": dz3, "dW3": dW3, "db3": db3,
                  "da2": da2, "dz2": dz2, "dW2": dW2, "db2": db2,
                  "da1": da1, "dz1": dz1, "dW1": dW1, "db1": db1}
-    
+
     return gradients
- 
+
+
 def update_parameters(parameters, grads, learning_rate):
     """
     Update parameters using gradient descent
@@ -139,16 +143,17 @@ def update_parameters(parameters, grads, learning_rate):
                   parameters['W' + str(i)] = ... 
                   parameters['b' + str(i)] = ...
     """
-    
-    L = len(parameters) // 2 # number of layers in the neural networks
- 
+
+    L = len(parameters) // 2  # number of layers in the neural networks
+
     # Update rule for each parameter
     for k in range(L):
-        parameters["W" + str(k+1)] = parameters["W" + str(k+1)] - learning_rate * grads["dW" + str(k+1)]
-        parameters["b" + str(k+1)] = parameters["b" + str(k+1)] - learning_rate * grads["db" + str(k+1)]
-        
+        parameters["W" + str(k + 1)] = parameters["W" + str(k + 1)] - learning_rate * grads["dW" + str(k + 1)]
+        parameters["b" + str(k + 1)] = parameters["b" + str(k + 1)] - learning_rate * grads["db" + str(k + 1)]
+
     return parameters
-    
+
+
 def predict(X, y, parameters):
     """
     This function is used to predict the results of a  n-layer neural network.
@@ -160,39 +165,43 @@ def predict(X, y, parameters):
     Returns:
     p -- predictions for the given dataset X
     """
-    
+
     m = X.shape[1]
-    p = np.zeros((1,m), dtype = np.int)
-    
+    p = np.zeros((1, m), dtype=np.int)
+
     # Forward propagation
     a3, caches = forward_propagation(X, parameters)
-    
+
     # convert probas to 0/1 predictions
     for i in range(0, a3.shape[1]):
-        if a3[0,i] > 0.5:
-            p[0,i] = 1
+        if a3[0, i] > 0.5:
+            p[0, i] = 1
         else:
-            p[0,i] = 0
- 
+            p[0, i] = 0
+
     # print results
-    print("Accuracy: "  + str(np.mean((p[0,:] == y[0,:]))))
-    
+    print("Accuracy: " + str(np.mean((p[0, :] == y[0, :]))))
+
     return p
-    
+
+
 def load_dataset(is_plot=True):
     np.random.seed(1)
-    train_X, train_Y = sklearn.datasets.make_circles(n_samples=300, noise=.05)
+    train_X, train_Y = sklearn.datasets.make_circles(n_samples=500, noise=.05)
     np.random.seed(2)
     test_X, test_Y = sklearn.datasets.make_circles(n_samples=100, noise=.05)
     # Visualize the data
+    a = train_X[:, 0]
+    b = train_X[:, 1]
     if is_plot:
-        plt.scatter(train_X[:, 0], train_X[:, 1], c=train_Y, s=40, cmap=plt.cm.Spectral);
+        plt.scatter(train_X[:, 0], train_X[:, 1], c=train_Y, s=20, cmap=plt.cm.Spectral)
     train_X = train_X.T
     train_Y = train_Y.reshape((1, train_Y.shape[0]))
     test_X = test_X.T
     test_Y = test_Y.reshape((1, test_Y.shape[0]))
     return train_X, train_Y, test_X, test_Y
- 
+
+
 def plot_decision_boundary(model, X, y):
     # Set min and max values and give it some padding
     x_min, x_max = X[0, :].min() - 1, X[0, :].max() + 1
@@ -209,7 +218,8 @@ def plot_decision_boundary(model, X, y):
     plt.xlabel('x1')
     plt.scatter(X[0, :], X[1, :], c=y, cmap=plt.cm.Spectral)
     plt.show()
- 
+
+
 def predict_dec(parameters, X):
     """
     Used for plotting decision boundary.
@@ -221,9 +231,8 @@ def predict_dec(parameters, X):
     Returns
     predictions -- vector of predictions of our model (red: 0 / blue: 1)
     """
-    
+
     # Predict using forward propagation and a classification threshold of 0.5
     a3, cache = forward_propagation(X, parameters)
-    predictions = (a3>0.5)
+    predictions = (a3 > 0.5)
     return predictions
-
