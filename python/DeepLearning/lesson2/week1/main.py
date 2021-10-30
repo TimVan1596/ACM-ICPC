d38580f568fc89db8778f5d3b015dea7141ff388
TimVan <877020296@qq.com>
2021-10-30 21:05:32 +0800

    L2W1

--- python/DeepLearning/lesson2/week1/main.py
+++ python/DeepLearning/lesson2/week1/main.py
@@ -260,8 +260,10 @@ def model(X, Y, learning_rate=0.3, num_iterations=30000, print_cost=True, is_plo
             ## 记录成本
             costs.append(cost)
             if (print_cost and i % 10000 == 0):
-                # 打印成本
-                print("第" + str(i) + "次迭代，成本值为：" + str(cost))
+                accuracy = getAccuracy(a3, Y)
+
+                print("第" + str(i) + "次迭代，成本值为："
+                      + str(round(cost, 10)) + "，准确性为" + str(accuracy) + "%")
 
     # 是否绘制成本曲线图
     if is_plot:
@@ -294,7 +296,8 @@ def compute_cost_with_regularization(A3, Y, parameters, lambd):
 
     cross_entropy_cost = reg_utils.compute_cost(A3, Y)
 
-    L2_regularization_cost = lambd * (np.sum(np.square(W1)) + np.sum(np.square(W2)) + np.sum(np.square(W3))) / (2 * m)
+    L2_regularization_cost = lambd * (np.sum(np.square(W1)) + np.sum(np.square(W2))
+                                      + np.sum(np.square(W3))) / (2 * m)
 
     cost = cross_entropy_cost + L2_regularization_cost
 
@@ -335,6 +338,125 @@ def backward_propagation_with_regularization(X, Y, cache, lambd):
     dZ1 = np.multiply(dA1, np.int64(A1 > 0))
     dW1 = (1 / m) * np.dot(dZ1, X.T) + ((lambd * W1) / m)
     db1 = (1 / m) * np.sum(dZ1, axis=1, keepdims=True)
+
+    gradients = {"dZ3": dZ3, "dW3": dW3, "db3": db3, "dA2": dA2,
+                 "dZ2": dZ2, "dW2": dW2, "db2": db2, "dA1": dA1,
+                 "dZ1": dZ1, "dW1": dW1, "db1": db1}
+
+    return gradients
+
+
+# dropout的方法
+def forward_propagation_with_dropout(X, parameters, keep_prob=0.5):
+    """
+    实现具有随机舍弃节点的前向传播。
+    LINEAR -> RELU + DROPOUT -> LINEAR -> RELU + DROPOUT -> LINEAR -> SIGMOID.
+
+    参数：
+        X  - 输入数据集，维度为（2，示例数）
+        parameters - 包含参数“W1”，“b1”，“W2”，“b2”，“W3”，“b3”的python字典：
+            W1  - 权重矩阵，维度为（20,2）
+            b1  - 偏向量，维度为（20,1）
+            W2  - 权重矩阵，维度为（3,20）
+            b2  - 偏向量，维度为（3,1）
+            W3  - 权重矩阵，维度为（1,3）
+            b3  - 偏向量，维度为（1,1）
+        keep_prob  - 随机删除的概率，实数
+    返回：
+        A3  - 最后的激活值，维度为（1,1），正向传播的输出
+        cache - 存储了一些用于计算反向传播的数值的元组
+    """
+    np.random.seed(1)
+
+    W1 = parameters["W1"]
+    b1 = parameters["b1"]
+    W2 = parameters["W2"]
+    b2 = parameters["b2"]
+    W3 = parameters["W3"]
+    b3 = parameters["b3"]
+
+    # LINEAR -> RELU -> LINEAR -> RELU -> LINEAR -> SIGMOID
+    Z1 = np.dot(W1, X) + b1
+    A1 = reg_utils.relu(Z1)
+
+    # 下面的步骤1-4对应于上述的步骤1-4。
+    D1 = np.random.rand(A1.shape[0], A1.shape[1])  # 步骤1：初始化矩阵D1 = np.random.rand(..., ...)
+    D1 = D1 < keep_prob  # 步骤2：将D1的值转换为0或1（使​​用keep_prob作为阈值）
+    A1 = A1 * D1  # 步骤3：舍弃A1的一些节点（将它的值变为0或False）
+    A1 = A1 / keep_prob  # 步骤4：缩放未舍弃的节点(不为0)的值
+    """
+    #不理解的同学运行一下下面代码就知道了。
+    import numpy as np
+    np.random.seed(1)
+    A1 = np.random.randn(1,3)
+
+    D1 = np.random.rand(A1.shape[0],A1.shape[1])
+    keep_prob=0.5
+    D1 = D1 < keep_prob
+    print(D1)
+
+    A1 = 0.01
+    A1 = A1 * D1
+    A1 = A1 / keep_prob
+    print(A1)
+    
+    结果是
+    [[ True False  True]]
+    [[0.02 0.   0.02]]
+    """
+
+    Z2 = np.dot(W2, A1) + b2
+    A2 = reg_utils.relu(Z2)
+
+    # 下面的步骤1-4对应于上述的步骤1-4。
+    D2 = np.random.rand(A2.shape[0], A2.shape[1])  # 步骤1：初始化矩阵D2 = np.random.rand(..., ...)
+    D2 = D2 < keep_prob  # 步骤2：将D2的值转换为0或1（使​​用keep_prob作为阈值）
+    A2 = A2 * D2  # 步骤3：舍弃A1的一些节点（将它的值变为0或False）
+    A2 = A2 / keep_prob  # 步骤4：缩放未舍弃的节点(不为0)的值
+
+    Z3 = np.dot(W3, A2) + b3
+    A3 = reg_utils.sigmoid(Z3)
+
+    cache = (Z1, D1, A1, W1, b1, Z2, D2, A2, W2, b2, Z3, A3, W3, b3)
+
+    return A3, cache
+
+
+def backward_propagation_with_dropout(X, Y, cache, keep_prob):
+    """
+    实现我们随机删除的模型的后向传播。
+    参数：
+        X  - 输入数据集，维度为（2，示例数）
+        Y  - 标签，维度为（输出节点数量，示例数量）
+        cache - 来自forward_propagation_with_dropout（）的cache输出
+        keep_prob  - 随机删除的概率，实数
+
+    返回：
+        gradients - 一个关于每个参数、激活值和预激活变量的梯度值的字典
+    """
+    m = X.shape[1]
+    (Z1, D1, A1, W1, b1, Z2, D2, A2, W2, b2, Z3, A3, W3, b3) = cache
+
+    dZ3 = A3 - Y
+    dW3 = (1 / m) * np.dot(dZ3, A2.T)
+    db3 = 1. / m * np.sum(dZ3, axis=1, keepdims=True)
+    dA2 = np.dot(W3.T, dZ3)
+
+    dA2 = dA2 * D2  # 步骤1：使用正向传播期间相同的节点，舍弃那些关闭的节点（因为任何数乘以0或者False都为0或者False）
+    dA2 = dA2 / keep_prob  # 步骤2：缩放未舍弃的节点(不为0)的值
+
+    dZ2 = np.multiply(dA2, np.int64(A2 > 0))
+    dW2 = 1. / m * np.dot(dZ2, A1.T)
+    db2 = 1. / m * np.sum(dZ2, axis=1, keepdims=True)
+
+    dA1 = np.dot(W2.T, dZ2)
+
+    dA1 = dA1 * D1  # 步骤1：使用正向传播期间相同的节点，舍弃那些关闭的节点（因为任何数乘以0或者False都为0或者False）
+    dA1 = dA1 / keep_prob  # 步骤2：缩放未舍弃的节点(不为0)的值
+
+    dZ1 = np.multiply(dA1, np.int64(A1 > 0))
+    dW1 = 1. / m * np.dot(dZ1, X.T)
+    db1 = 1. / m * np.sum(dZ1, axis=1, keepdims=True)
 
     gradients = {"dZ3": dZ3, "dW3": dW3, "db3": db3, "dA2": dA2,
                  "dZ2": dZ2, "dW2": dW2, "db2": db2, "dA1": dA1,
@@ -391,13 +513,26 @@ if __name__ == '__main__':
     # axes.set_xlim([-0.75, 0.40])
     # axes.set_ylim([-0.75, 0.65])
     # reg_utils.plot_decision_boundary(lambda x: reg_utils.predict_dec(parameters, x.T), train_X, train_Y)
-    parameters = model(train_X, train_Y, lambd=0.7, is_plot=True)
-    print("使用正则化，训练集:")
-    predictions_train = reg_utils.predict(train_X, train_Y, parameters)
-    print("使用正则化，测试集:")
-    predictions_test = reg_utils.predict(test_X, test_Y, parameters)
+    # parameters = model(train_X, train_Y, lambd=0.7, is_plot=True)
+    # print("使用正则化，训练集:")
+    # predictions_train = reg_utils.predict(train_X, train_Y, parameters)
+    # print("使用正则化，测试集:")
+    # predictions_test = reg_utils.predict(test_X, test_Y, parameters)
+    #
+    # plt.title("Model with L2-regularization")
+    # axes = plt.gca()
+    # axes.set_xlim([-0.75, 0.40])
+    # axes.set_ylim([-0.75, 0.65])
+    # reg_utils.plot_decision_boundary(lambda x: reg_utils.predict_dec(parameters, x.T), train_X, train_Y)
 
-    plt.title("Model with L2-regularization")
+    parameters = model(train_X, train_Y, keep_prob=0.86, learning_rate=0.3, is_plot=True)
+
+    print("使用随机删除节点，训练集:")
+    predictions_train = reg_utils.predict(train_X, train_Y, parameters)
+    print("使用随机删除节点，测试集:")
+    reg_utils.predictions_test = reg_utils.predict(test_X, test_Y, parameters)
+
+    plt.title("Model with dropout")
     axes = plt.gca()
     axes.set_xlim([-0.75, 0.40])
     axes.set_ylim([-0.75, 0.65])
--- python/DeepLearning/test.py
+++ python/DeepLearning/test.py
@@ -8,11 +8,16 @@ def sigmoid(x):
 
 
 if __name__ == '__main__':
-    a = np.array([[1, 2, 3]])
-    assert a.shape == (1, 3)
+    np.random.seed(1)
+    A1 = np.random.randn(1, 3)
+    print(A1)
 
-    a = np.squeeze(a)
-    print(a.shape)
+    D1 = np.random.rand(A1.shape[0], A1.shape[1])
+    print(D1)
+    keep_prob = 0.5
+    D1 = D1 < keep_prob
+    print(D1)
 
-    ret = sigmoid(np.array([-10000000, -4, -3, -2, -1, 0, 1, 2, 3, 4]))
-    print(ret)
+    A1 = A1 * D1
+    # A1 = A1 / keep_prob
+    print(A1)
