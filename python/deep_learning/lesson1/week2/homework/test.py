03bc414706fc644a0dec242bf6def6b2d462567e
TimVan <877020296@qq.com>
2021-11-05 20:58:43 +0800

    L2W1

--- python/deep_learning/lesson1/week2/homework/test.py
+++ python/deep_learning/lesson1/week2/homework/test.py
@@ -1,8 +1,36 @@
-# -*- coding:utf-8 -*-
-# @Time:2021/8/27 13:35
-# @Author:TimVan
-# @File:app.py
-# @Software:PyCharm
+import h5py
+import numpy as np
+from matplotlib import pyplot as plt
+
+file = h5py.File('datasets/train_catvnoncat.h5', 'r')
+print(file.keys())
+
+# keys = file.keys()
+# for key_name in keys:
+#     value = file[key_name]
+#     print('key_name = ')
+#     print(key_name)
+#     print('value = ')
+#     print(value)
+#     print('-' * 10)
 
 
-print("hello")
+key_name = 'list_classes'
+value = file[key_name]
+
+print('key_name = ')
+print(key_name)
+print('value = ')
+print(value[0])
+print(value[1])
+print('-' * 10)
+
+key_name = 'train_set_x'
+value = file[key_name]
+print(value)
+train_set_x = value
+print(train_set_x.shape)
+
+arr = np.array(train_set_x)
+plt.imshow(arr[2])
+plt.show()
---
+++ python/deep_learning/lesson2/test.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+if __name__ == '__main__':
+    a = np.arange(0, 11, 2)
+    a = a.reshape(2, 3)
+    print(a)
