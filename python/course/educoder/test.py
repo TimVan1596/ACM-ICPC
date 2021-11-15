7beeb22890662f06d7ba49e5cfa7aa37ac68009f
TimVan <877020296@qq.com>
2021-11-15 23:00:50 +0800

    正则

--- python/course/educoder/test.py
+++ python/course/educoder/test.py
@@ -1,10 +1,7 @@
 import re
-text = 'Love your parents. We are too busy growing up yet we forget that they are already growing.'
-#********** Begin *********#
-#1.匹配字符单词 Love
-print(re.findall(r'Love',text))
-#2.匹配以 w 开头的完整单词
-print(re.findall(r'\b[w][a-zA-Z]{0,}\b',text))
-#3.查找三个字母长的单词（提示：可以使用{m,n}方式）
-print(re.findall(r'\b[a-zA-Z]{3}\b',text))
-#********** End **********#
+
+itext = re.finditer(r'\d+','12 edueduedu44coder deducoder, 11skdh   ds 12')      #匹配所有的数字
+for i in itext:
+    print(i)
+    print(i.group())
+    print(i.span())   #span()返回一个元组包含匹配 (开始,结束) 的位置
