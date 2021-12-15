dec25b0094986eda6a87edaf4493bb2ce1c718b3
TimVan <877020296@qq.com>
2021-12-15 21:11:22 +0800

    L2W2：mini-batch的打乱

--- python/deep_learning/lesson2/week2/test.py
+++ python/deep_learning/lesson2/week2/test.py
@@ -3,9 +3,15 @@ import numpy as np
 
 # 将数据集切分成t份，便于实现mini-batch
 def split_data(dataset: list, t=1):
+    x = dataset[:][0]
+    y = dataset[:][1]
+
+    state = np.random.get_state()
+    np.random.shuffle(x)
+    np.random.set_state(state)
+    np.random.shuffle(y)
+
     for i in range(len(dataset)):
-        x = dataset[i][0]
-        y = dataset[i][1]
         print(x)
         print(y)
     # np.shuffle(dataset)
@@ -13,7 +19,7 @@ def split_data(dataset: list, t=1):
 
 if __name__ == '__main__':
     x = np.linspace(1, 10, 10, dtype='int').reshape(2, 5)
-    y = [0, 0, 1, 1, 1]
+    y = [x[0, i] + x[1, i] for i in range(len(x[0,]))]
     print(x)
     print(y)
     dataset = [(x[:, i], y[i]) for i in range(len(y))]
