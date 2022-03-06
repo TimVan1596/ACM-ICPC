ad781bbd83bab14c5a8a5f54dee77ea6a2127e15
Timvan <877020296@qq.com>
2022-03-06 17:12:27 +0800

    Vue 更新时的一个问题.html

--- python/deep_learning/test.py
+++ python/deep_learning/test.py
@@ -5,16 +5,17 @@ import numpy as np
 def sigmoid(x):
     s = 1 / (1 + np.exp(-x))
     return s
-
+ 
 
 if __name__ == '__main__':
-    keys = []
-    key = 'W1'
-    parameter = {
-        'W1': np.array([
-            [0, 1, 2], [3, 4, 5]
-        ])
-    }
-    new_vector = np.reshape(parameter[key], (-1, 1))
-    keys = keys + [key] * new_vector.shape[0]
-    print(keys)
+    x = np.arange(0, 9).reshape([3, 3])
+    y = np.array([[0], [1], [2]])
+    print(x.shape)
+    print(y.shape)
+
+    # [[[0 1] [0 2] [0 3]]
+    # [[1 4] [1 5] [1 6]]
+    # [[2 7] [2 8] [2 9]]]
+    z = [[[y[i][0],x[i,j]] for j in range(x.shape[1])] for i in range(x.shape[0])]
+    print(z.shape)
+    print(z)
