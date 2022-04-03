e5e52bb12bc6ee7a1b9773c2e12f036daaa04258
Timvan <877020296@qq.com>
2022-04-03 12:42:35 +0800

    tensorflow 完成三层神经网络

--- python/deep_learning/TensorFlow/第四章 Tensorflow基础/4.10 前向传播实战.py
+++ python/deep_learning/TensorFlow/第四章 Tensorflow基础/4.10 前向传播实战.py
@@ -1,5 +1,9 @@
+import matplotlib.pyplot as plt
 import tensorflow as tf
 import tensorflow.keras.datasets as datasets
+
+plt.rcParams['font.sans-serif'] = ['SimHei']  # 显示中文标签
+plt.rcParams['axes.unicode_minus'] = False  # 这两行需要手动设置
 
 
 # 1、获取数据集
@@ -43,9 +47,10 @@ def init_parameters():
 # 1 epoch=一代=只是一次遍历了训练集
 def train_epoch(epoch, train_dataset, param, lr=0.001):
     w1, b1, w2, b2, w3, b3 = param[0], param[1], param[2], param[3], param[4], param[5]
-    print(tensor_equal(w1, param[0]))
-    print(tensor_equal(w1, param[1]))
-    print(tensor_equal(b1, param[1]))
+    print('w1.shape=')
+    print(w1.shape)
+    print('b2.shape=')
+    print(b2.shape)
 
     # 总共300个step，每个step有200个数据
     # 300*200 = 600000 =  MNIST数据集数量
@@ -53,11 +58,35 @@ def train_epoch(epoch, train_dataset, param, lr=0.001):
         # x.shape=(200, 784)  y.shape=(200, 10)
         with tf.GradientTape() as tape:
             # 三层结构为 784*256,256*128,128*10
-            h1 = x @ w1 + tf.broadcast_to(b1, (x.shape[0], 256))
-            pass
+            # 第一层 (200, 784)*(784, 256)+(200,256) = (200,256)
+            h1 = x @ w1 + b1
+            h1 = tf.nn.relu(h1)
+            # 第二层 (200,256)*(256, 128)+(200,128) = (200,128)
+            h2 = h1 @ w2 + b2
+            h2 = tf.nn.relu(h2)
+            # 输出层 (200,128)*(128, 10)+(200,10) = (200,10)
+            h3 = h2 @ w3 + b3
+            out = tf.nn.relu(h3)
+            # 计算均方差 mse = mean(sum(y-out)^2)
+            loss = tf.square(y - out)
+            loss = tf.reduce_mean(loss)
+            grads = tape.gradient(loss, [w1, b1, w2, b2, w3, b3])
+        # 梯度更新，原地更新
+        w1.assign_sub(grads[0] * lr)
+        b1.assign_sub(grads[1] * lr)
+        w2.assign_sub(grads[2] * lr)
+        b2.assign_sub(grads[3] * lr)
+        w3.assign_sub(grads[4] * lr)
+        b3.assign_sub(grads[5] * lr)
+        if step % 200 == 0:
+            tips = "[debug]:epoch={0},step={1} => loss:{2}" \
+                .format(epoch, step, loss.numpy())
+            print(tips)
+    return loss.numpy()
 
 
 def train(epochs):
+    losses = []
     # 1、获取数据集
     train_dataset = load_data(batch_size=200)
     # 2、初始化参数
@@ -65,9 +94,18 @@ def train(epochs):
     param = (w1, b1, w2, b2, w3, b3)
     # 3、epoch进行训练
     for epoch in range(epochs):
-        train_epoch(epoch, train_dataset, param, lr=0.001)
-        break
-    pass
+        loss = train_epoch(epoch, train_dataset, param, lr=0.001)
+        losses.append(loss)
+    x = range(0, epochs)
+    # 绘制曲线
+    plt.plot(x, losses, color='orange',
+             marker='s', label='训练')
+    plt.xlabel('Epoch')
+    plt.ylabel('MSE')
+    plt.legend()
+    plt.savefig('MNIST数据集的前向传播训练误差曲线.jpg')
+    plt.show()
+    plt.close()
 
 
 # 判断两个tensor的值是否相等
@@ -88,5 +126,5 @@ def tensor_equal(a, b):
 
 
 if __name__ == '__main__':
-    epochs = 5
+    epochs = 32
     train(epochs)
